41fcc82ba26b45e02260022ef6174c1e8e780073
Pete West <peterwest86@gmail.com>
2018-03-27 12:59:09 +0200

    added CircleCI files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-52-57-68-23.eu-central-1.compute.amazonaws.com',
       key: '~/.ssh/aws-ubuntu-linux.pem',
-      ref: 'origin/master',
+      ref: 'origin/dev',
       repo: 'https://github.com/peterwest-1/peterwestmedia',
       path: '/home/ubuntu/server',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
