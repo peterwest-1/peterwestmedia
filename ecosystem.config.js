3cf3cd172488b688d9973de897e0b66d3d507086
Pete West <peterwest86@gmail.com>
2018-09-13 11:08:43 +0200

    Updated relevate directories

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-196-182-115.eu-central-1.compute.amazonaws.com',
-      key: 'C://Users//simon//Documents//Programming//AWS//aws-pwm.pem',
+      host: 'ec2-18-185-17-38.eu-central-1.compute.amazonaws.com',
+      key: 'C://Users//simon//Documents//Programming//Websites//Keys//AWS-Webserver-KepPair.pem',
       ref: 'origin/master',
       repo: 'https://github.com/peterwest-1/peterwestmedia',
       path: '/home/ubuntu/server',
